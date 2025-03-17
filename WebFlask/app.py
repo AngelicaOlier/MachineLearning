cd2d9741fdf126f99c2fcddb74cb16a92cfec2b9
Angelica Olier <aolier@ucundinamarca.edu.co>
2025-03-16 20:58:31 -0500

    Caso de Uso
    Caso de uso supervisado

--- WebFlask/app.py
+++ WebFlask/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+import re
+from datetime import datetime
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/MlCasoDeUsoSupervisado')
+def MlCaso():
+    return render_template('MlCasoDeUsoSupervisado.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
