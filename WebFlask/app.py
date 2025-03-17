d6037410120959aa9e7bbfab695c34aad8ec7399
Johan Martinez <64931268+Lando18tinez@users.noreply.github.com>
2025-03-16 22:24:56 -0500

    agregar carge de repo con regresion lineal

--- WebFlask/app.py
+++ WebFlask/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
-
+#pip install -r requirements.txt
+#flask --app app run --debug
+import re
+from datetime import datetime
+from flask import Flask, render_template, request
+import RL_SalarioExperiencia
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +12,19 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route("/linearRegressionExpSalario", methods=["GET", "POST"])
+def linear_regression_Exp_Salario():
+    predicted_result = None
+    graph_image = RL_SalarioExperiencia.generate_graph()
+
+    if request.method == "POST":
+        try:
+            experiencia = float(request.form.get("Experiencia"))
+            predicted_result = RL_SalarioExperiencia.model.predict([[experiencia]])[0]
+        except ValueError:
+            predicted_result = "Entrada no válida"
+
+    return render_template("linearRegressionExpSalario.html",
+                           result=predicted_result,
+                           graph=graph_image)
